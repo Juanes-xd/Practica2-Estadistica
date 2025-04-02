f11d48d15ccc983a7f0bd786a82e6c7bb8241334
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-04-02 15:31:59 -0500

    Nuevos scripts para nuevas variables y sus respecctivos resultados

--- Script2.R
+++ Script2.R
@@ -10,13 +10,13 @@ resumen <- summary(datos)
 View(resumen)
 
 #Histogramas
-hist(datos$PIB, main="Histograma del PIB per cápita", xlab="PIB per cápita")
+hist(datos$Apoyo, main="Histograma del Apoyo per cápita", xlab="Apoyo per cápita")
 hist(datos$Apoyo, main="Histograma del Apoyo social", xlab="Apoyo social")
 hist(datos$Esperanza_vida, main="Histograma de la esperanza de vida", xlab="Esperanza de vida")
 hist(datos$Felicidad, main="Histograma de felicidad", xlab="Felicidad")
 
 #Graficos de cajas
-boxplot(datos$PIB, main="Grafico del PIB per cápita")
+boxplot(datos$Apoyo, main="Grafico del Apoyo per cápita")
 boxplot(datos$Corrupcion, main="Grafico de corrupcion")
 
 #Variables
@@ -24,36 +24,36 @@ datos$Felicidad <- as.numeric(datos$Felicidad)  # Felicidad (X)
 datos$Corrupcion <- as.numeric(datos$Corrupcion)  # Corrupcion (Y)
 datos$Esperanza_vida <- as.numeric(datos$Esperanza_vida)  # Esperanza_vida (Y)
 datos$Apoyo <- as.numeric(datos$Apoyo)  # Apoyo(Y)
-datos$PIB <- as.numeric(datos$PIB)  # PIB (Y)
-datos$Generosidad <- as.numeric(datos$Generosidad)  # PIB (Y)
-datos$Libertad <- as.numeric(datos$Libertad)  # PIB (Y)
+datos$Apoyo <- as.numeric(datos$Apoyo)  # Apoyo (Y)
+datos$Generosidad <- as.numeric(datos$Generosidad)  # Apoyo (Y)
+datos$Libertad <- as.numeric(datos$Libertad)  # Apoyo (Y)
 
 # 1. Gráfico de dispersión
-plot(datos$Felicidad,datos$PIB, 
+plot(datos$Felicidad,datos$Apoyo, 
      xlab = "Felicidad", 
-     ylab = "PIB", 
-     main = "PIB vs Felicidad")
-abline(lm(datos$PIB~datos$Felicidad,data = datos),col="red")
+     ylab = "Apoyo", 
+     main = "Apoyo vs Felicidad")
+abline(lm(datos$Apoyo~datos$Felicidad,data = datos),col="red")
 
 #Con ggplot2
-ggplot(datos, aes(x = Felicidad, y = PIB)) +
+ggplot(datos, aes(x = Felicidad, y = Apoyo)) +
   geom_point(color = "blue", alpha = 0.7) +
   geom_smooth(method = "lm", se = TRUE, color = "red") +
-  labs(title = "PIB vs Felicidad",
+  labs(title = "Apoyo vs Felicidad",
        x = "Felicidad",
-       y = "PIB") +
+       y = "Apoyo") +
   theme_minimal()
 
 # 2. Matriz de correlación
-cor_matrix <- cor(datos[, c("Felicidad", "PIB")])
+cor_matrix <- cor(datos[, c("Felicidad", "Apoyo")])
 cor_matrix
 
 #Con ggpairs 
-ggcor <- ggpairs(datos[, c("Felicidad", "PIB")])
+ggcor <- ggpairs(datos[, c("Felicidad", "Apoyo")])
 ggcor
 
 # 3. Ajuste del modelo de regresión
-modelo <- lm(PIB ~ Felicidad, data = datos)
+modelo <- lm(Apoyo ~ Felicidad, data = datos)
 
 # 4. Resumen del modelo
 summary(modelo)
@@ -65,10 +65,10 @@ r2 <-(cor_matrix[1,2])^{2}
 
 #Gráfico de dispersión con la ecuación de la recta encontrada
 coeficientes <- coef(modelo)
-plot(datos$Felicidad, datos$PIB, 
+plot(datos$Felicidad, datos$Apoyo, 
      xlab = "Felicidad", 
-     ylab = "PIB", 
-     main = "PIB vs Felicidad")
+     ylab = "Apoyo", 
+     main = "Apoyo vs Felicidad")
 abline(modelo, col = "red", lwd = 2)
 eq_text <- paste0("y = ", round(coeficientes[1], 2), " + ", round(coeficientes[2], 2), "x")
 legend("topright", legend = eq_text, col = "red", lty = 1, bty = "o") #si cambiana n por o se agrega un cuadro
--- Script3.R
+++ Script3.R
@@ -10,13 +10,13 @@ resumen <- summary(datos)
 View(resumen)
 
 #Histogramas
-hist(datos$PIB, main="Histograma del PIB per cápita", xlab="PIB per cápita")
+hist(datos$Libertad, main="Histograma del Libertad per cápita", xlab="Libertad per cápita")
 hist(datos$Apoyo, main="Histograma del Apoyo social", xlab="Apoyo social")
 hist(datos$Esperanza_vida, main="Histograma de la esperanza de vida", xlab="Esperanza de vida")
 hist(datos$Felicidad, main="Histograma de felicidad", xlab="Felicidad")
 
 #Graficos de cajas
-boxplot(datos$PIB, main="Grafico del PIB per cápita")
+boxplot(datos$Libertad, main="Grafico del Libertad per cápita")
 boxplot(datos$Corrupcion, main="Grafico de corrupcion")
 
 #Variables
@@ -24,36 +24,36 @@ datos$Felicidad <- as.numeric(datos$Felicidad)  # Felicidad (X)
 datos$Corrupcion <- as.numeric(datos$Corrupcion)  # Corrupcion (Y)
 datos$Esperanza_vida <- as.numeric(datos$Esperanza_vida)  # Esperanza_vida (Y)
 datos$Apoyo <- as.numeric(datos$Apoyo)  # Apoyo(Y)
-datos$PIB <- as.numeric(datos$PIB)  # PIB (Y)
-datos$Generosidad <- as.numeric(datos$Generosidad)  # PIB (Y)
-datos$Libertad <- as.numeric(datos$Libertad)  # PIB (Y)
+datos$Libertad <- as.numeric(datos$Libertad)  # Libertad (Y)
+datos$Generosidad <- as.numeric(datos$Generosidad)  # Libertad (Y)
+datos$Libertad <- as.numeric(datos$Libertad)  # Libertad (Y)
 
 # 1. Gráfico de dispersión
-plot(datos$Felicidad,datos$PIB, 
+plot(datos$Felicidad,datos$Libertad, 
      xlab = "Felicidad", 
-     ylab = "PIB", 
-     main = "PIB vs Felicidad")
-abline(lm(datos$PIB~datos$Felicidad,data = datos),col="red")
+     ylab = "Libertad", 
+     main = "Libertad vs Felicidad")
+abline(lm(datos$Libertad~datos$Felicidad,data = datos),col="red")
 
 #Con ggplot2
-ggplot(datos, aes(x = Felicidad, y = PIB)) +
+ggplot(datos, aes(x = Felicidad, y = Libertad)) +
   geom_point(color = "blue", alpha = 0.7) +
   geom_smooth(method = "lm", se = TRUE, color = "red") +
-  labs(title = "PIB vs Felicidad",
+  labs(title = "Libertad vs Felicidad",
        x = "Felicidad",
-       y = "PIB") +
+       y = "Libertad") +
   theme_minimal()
 
 # 2. Matriz de correlación
-cor_matrix <- cor(datos[, c("Felicidad", "PIB")])
+cor_matrix <- cor(datos[, c("Felicidad", "Libertad")])
 cor_matrix
 
 #Con ggpairs 
-ggcor <- ggpairs(datos[, c("Felicidad", "PIB")])
+ggcor <- ggpairs(datos[, c("Felicidad", "Libertad")])
 ggcor
 
 # 3. Ajuste del modelo de regresión
-modelo <- lm(PIB ~ Felicidad, data = datos)
+modelo <- lm(Libertad ~ Felicidad, data = datos)
 
 # 4. Resumen del modelo
 summary(modelo)
@@ -65,10 +65,10 @@ r2 <-(cor_matrix[1,2])^{2}
 
 #Gráfico de dispersión con la ecuación de la recta encontrada
 coeficientes <- coef(modelo)
-plot(datos$Felicidad, datos$PIB, 
+plot(datos$Felicidad, datos$Libertad, 
      xlab = "Felicidad", 
-     ylab = "PIB", 
-     main = "PIB vs Felicidad")
+     ylab = "Libertad", 
+     main = "Libertad vs Felicidad")
 abline(modelo, col = "red", lwd = 2)
 eq_text <- paste0("y = ", round(coeficientes[1], 2), " + ", round(coeficientes[2], 2), "x")
 legend("topright", legend = eq_text, col = "red", lty = 1, bty = "o") #si cambiana n por o se agrega un cuadro
